3344b9a5e5f0ed946e9cd02b424c8d789f289037
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2022-05-30 18:08:56 -0300

    test

---
+++ app/backend/src/interfaces/index.ts
@@ -0,0 +1,8 @@
+interface ILoginPayload {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+}
+
+export default ILoginPayload;
---
+++ app/backend/src/tests/mocks/index.ts
@@ -0,0 +1,9 @@
+const userMocked = {
+  id: 1,
+  username: 'stark',
+  role: 'bilionario industrial',
+  email: 'teste@tester.com',
+  password: 'v1ng4d0rm41sf0rt3',
+};
+
+export default userMocked;
