abf0d0d721bff78cfbe31e0f106b508251422ab7
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2022-05-31 16:06:52 -0300

    refactoring

--- app/backend/src/database/models/MatchesModel.ts
+++ app/backend/src/database/models/MatchesModel.ts
@@ -53,10 +53,10 @@ Matches.init(
   },
 );
 
-Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'homeTeam' });
-Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'awayTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
 
-Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'homeTeam' });
-Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'awayTeam' });
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'teamHome' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'teamAway' });
 
 export default Matches;
