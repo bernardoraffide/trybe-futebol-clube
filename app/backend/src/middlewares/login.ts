e6d4180750c63d6347b499c65d6a543f898786f3
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2022-06-23 11:13:37 -0300

    new: brcypt added in password validation

--- app/backend/src/middlewares/login.ts
+++ app/backend/src/middlewares/login.ts
@@ -1,11 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
+import { compareSync } from 'bcryptjs';
+import Users from '../database/models/UsersModel';
 
-const validateLogin = (req: Request, res: Response, next: NextFunction) => {
+const validateLogin = async (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ message: 'All fields must be filled' });
+  const loginUser = await Users.findOne({ where: { email } });
+
+  if (!loginUser) {
+    return res.status(401).json({ message: 'User not found' });
   }
+
+  if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
 
   if (typeof email !== 'string' || typeof password !== 'string') {
     return res.status(401).json({ message: 'Invalid email or passwword' });
@@ -13,12 +19,14 @@ const validateLogin = (req: Request, res: Response, next: NextFunction) => {
 
   const regex = /\S+@\S+.\S\.+com/;
 
-  if (!regex.test(email)) {
-    return res.status(401).json({ message: 'Incorrect email or password' });
-  }
+  if (!regex.test(email)) return res.status(401).json({ message: 'Incorrect email or password' });
 
   if (password.length <= 6) {
     return res.status(401).json({ message: 'Password must have at least 6 characters' });
+  }
+
+  if (!compareSync(password, loginUser.password)) {
+    return res.status(401).json({ message: 'Invalid email or passwword' });
   }
 
   next();
